2aab4bc96d77ad9ac3d0badd9050908f6ea36695
Maryess <121576046+Maryess@users.noreply.github.com>
2024-03-15 10:02:39 +0300

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,6 @@
-import { ABeeZee, Inter } from 'next/font/google';
+import { ABeeZee, Inter .Lusitana} from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const abeezee = ABeeZee({ subsets: ['latin'], weight: '400' });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '700' });
